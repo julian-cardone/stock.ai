2624d6dce15327b79ee1609b60fd5b0b9b7a64a7
Julian Cardone <juliancardone1@gmail.com>
2023-07-24 14:45:29 -0400

    postgresql setup

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,15 @@
 from flask import Flask
-from dotenv import load_dotenv
 import os
+import psychopg2
+from dotenv import load_dotenv
+
+# # Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+conneciton = psychopg2.connect(url)
+
+@app.get('/')
+def home():
+    return "hello, world!"
