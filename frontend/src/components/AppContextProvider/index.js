da029d2e8fd4eb7eff2d9bdd08ee16d6c54df1a4
Julian Cardone <juliancardone1@gmail.com>
2023-07-25 21:19:14 -0400

    refactor

--- frontend/src/components/AppContextProvider/index.js
+++ frontend/src/components/AppContextProvider/index.js
@@ -1,22 +1,20 @@
-import { useRef, useState } from "react"
-import { AppContext } from "../../utils/context"
-import React from 'react';
+import { useRef, useState } from "react";
+import { AppContext } from "../../utils/context";
+import React from "react";
 
 export const AppContextProvider = ({ children }) => {
   // intialize cache
-  const cache = useRef(new Map())
-  // 
-  const [error, setError] = useState("")
+  const cache = useRef(new Map());
+  //
+  const [error, setError] = useState("");
 
   return (
     <AppContext.Provider value={{ setError, cache }}>
       {error ? (
-        <div className="AppError">
-        Something broke... Error: {error}
-        </div>
+        <div className="AppError">Something broke... Error: {error}</div>
       ) : (
         children
       )}
     </AppContext.Provider>
-  )
-}
+  );
+};
