e91e5548b5db31e9d5b4e45e4f3595fe7acf0fd1
Julian Cardone <juliancardone1@gmail.com>
2023-08-01 17:43:37 -0400

    session working on backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session, make_response
 from db import db_setup
 from db.models.instance import Instance
 
@@ -8,14 +8,23 @@ db = db_setup.setup_db()
 
 @app.route('/api/create', methods=['POST'])
 def create_instance():
-
     user_instance = Instance()
 
-    query = "INSERT INTO Instance (token_id) VALUES (%s) RETURNING token_id;" 
-    values = (user_instance.create(),)
+    with db, db.cursor() as cursor:
+        while True:
+            session_key = user_instance.create()  # Generate a new session token
+            query = "SELECT token_id FROM Instance WHERE token_id = %s;"
+            cursor.execute(query, (session_key,))
+            existing_token = cursor.fetchone()
+            if not existing_token:  # Token does not exist in the database
+                break
 
-    with db:
-        with db.cursor() as cursor:
-            cursor.execute(query, values)
-            token_id = cursor.fetchone()[0]
-    return {"token": token_id}, 201
+        query = "INSERT INTO Instance (token_id) VALUES (%s) RETURNING token_id;" 
+        cursor.execute(query, (session_key,))
+        token_id = cursor.fetchone()[0]
+
+    response = make_response('Session Created')
+    response.set_cookie('session_key', session_key)  # Set the session key as the cookie value
+
+    return response
+
