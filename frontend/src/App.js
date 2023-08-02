3b5023ae559058d16f3f646436badac4679664aa
Julian Cardone <juliancardone1@gmail.com>
2023-08-01 20:41:19 -0400

    hooks working for session. create app.js button and test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useStorageKey } from "./hooks/useSotrageKey";
 
 function App() {
 
+  const { data: storageKey, ...storageKeyUtils } = useStorageKey();
+
   return (
     <div className="App">
+      <button onClick={() => storageKeyUtils.createSession()}>begin session</button>
     </div>
   );
 }
---
+++ frontend/src/hooks/useSotrageKey.js
@@ -0,0 +1,29 @@
+import { useCallback, useState } from "react";
+import { useWrappedRequest } from "./useWrappedRequest";
+
+export function useStorageKey(){
+  const [storageKey, setStorageKey] = useState(localStorage.getItem("stock-ai-session-token") || null);
+  const { loading, wrappedRequest } = useWrappedRequest();
+
+  const createSession = useCallback(async () => {
+    if (storageKey){
+      wrappedRequest(async () =>{
+        const res = await fetch('/start_session', {
+          method: "POST",
+        })
+        const data = await res.json();
+        const sessionToken = data.session_token;
+        localStorage.setItem("stock-ai-session-token", sessionToken);
+        setStorageKey(sessionToken)
+      })
+    }
+  },[storageKey, wrappedRequest])
+
+  const invalidateSession = useCallback(async () => {
+    localStorage.removeItem("stock-ai-session-token");
+    setStorageKey(null)
+  },[])
+
+  return { data: loading, createSession, invalidateSession }
+
+}
