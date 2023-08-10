2761e116d94131ce057f185810e8412f9e66a413
Julian Cardone <juliancardone1@gmail.com>
2023-08-10 07:58:36 -0400

    settimeout for the user message

--- frontend/src/components/BusinessPage/UserNote.js
+++ frontend/src/components/BusinessPage/UserNote.js
@@ -16,7 +16,9 @@ function UserNote() {
         onClick={handleToggle}
       >
         <div className="col-12 bg-secondary text-white text-center py-2">
-          {open ? "Thank you for stopping by. As I work to address the limitations of the free API for fundamental data, the only available model will be Historical Operating Assumptions. This model has proven to be consistently reliable during testing, with the exception of minor discrepancies observed in the depreciation and amortization figures for specific companies." : "A note to the user:"}
+          {open
+            ? "Thank you for stopping by. As I work to address the limitations of the free API for fundamental data, the only available model will be Historical Operating Assumptions. This model has proven to be consistently reliable during testing, with the exception of minor discrepancies observed in the depreciation and amortization figures for specific companies."
+            : "A note to the user:"}
         </div>
       </div>
     </>
