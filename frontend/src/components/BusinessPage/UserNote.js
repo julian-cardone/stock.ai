4a5a3baacae30774acbbc6b493b112c67c1790b6
Julian Cardone <juliancardone1@gmail.com>
2023-08-10 12:02:19 -0400

    deploygit add .

--- frontend/src/components/BusinessPage/UserNote.js
+++ frontend/src/components/BusinessPage/UserNote.js
@@ -3,9 +3,17 @@ import "./usernote.css";
 
 function UserNote() {
   const [open, setOpen] = useState(false);
+  const [text, setText] = useState("A note to the user:");
 
   const handleToggle = () => {
     setOpen(!open);
+    open
+      ? setText("A note to the user:")
+      : setTimeout(() => {
+          setText(
+            "Thank you for stopping by. As I work to address the limitations of the free API for fundamental data, the only available model will be Historical Operating Assumptions. This model has proven to be consistently reliable during testing, with the exception of minor discrepancies observed in the depreciation and amortization figures for specific companies."
+          );
+        }, 250);
   };
 
   return (
@@ -16,9 +24,7 @@ function UserNote() {
         onClick={handleToggle}
       >
         <div className="col-12 bg-secondary text-white text-center py-2">
-          {open
-            ? "Thank you for stopping by. As I work to address the limitations of the free API for fundamental data, the only available model will be Historical Operating Assumptions. This model has proven to be consistently reliable during testing, with the exception of minor discrepancies observed in the depreciation and amortization figures for specific companies."
-            : "A note to the user:"}
+          <p className="mb-0 mx-3">{text}</p>
         </div>
       </div>
     </>
