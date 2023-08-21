0e3efb48e1d7481c8c03e7a8ff0287426f5d6b5a
Julian Cardone <juliancardone1@gmail.com>
2023-08-21 14:14:31 -0400

    navbar is good for now. deisgn home page

--- backend/frontend/src/components/Nav/index.js
+++ backend/frontend/src/components/Nav/index.js
@@ -6,62 +6,67 @@ function Nav() {
 
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light ps-lg-5 ps-md-3 ps-sm-1">
-        <div class="container-fluid">
-          <Link className="navbar-brand" to="/home">
-            Stock.ai
+      <nav className="navbar navbar-expand-lg navbar-light bg-light ps-lg-5 ps-md-3 ps-sm-1">
+        <div className="container-fluid">
+          <Link className="navbar-brand logo-text" to="/home">
+            stock.ai
           </Link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <form class="d-flex">
+          <form className="d-flex col-lg-5 col-md-5 col-sm-3 ms-5">
             <input
-              class="form-control me-2"
+              className="form-control me-2"
               type="search"
-              placeholder="Search"
+              placeholder="Enter a stock symbol or company name"
               aria-label="Search"
             />
-            <button class="btn btn-outline-success" type="submit">
+            <button className="btn btn-outline-success" type="submit">
               Search
             </button>
           </form>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link
-                  className={`nav-link ${
-                    location.pathname === "/home" ? "active" : ""
-                  }`}
-                  to="/home"
-                  aria-current={
-                    location.pathname === "/home" ? "page" : undefined
-                  }
-                >
-                  Home <span className="sr-only"></span>
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link
-                  className={`nav-link ${
-                    location.pathname === "/generate" ? "active" : ""
-                  }`}
-                  to="/generate"
-                  aria-current={
-                    location.pathname === "/generate" ? "page" : undefined
-                  }
-                >
-                  Generate
-                </Link>
-              </li>
-            </ul>
+          <div className="ms-auto px-5">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item px-2">
+                  <Link
+                    className={`nav-link ${
+                      location.pathname === "/home" ? "active" : ""
+                    }`}
+                    to="/home"
+                    aria-current={
+                      location.pathname === "/home" ? "page" : undefined
+                    }
+                  >
+                    Home <span className="sr-only"></span>
+                  </Link>
+                </li>
+                <li className="nav-item px-2">
+                  <Link
+                    className={`nav-link ${
+                      location.pathname === "/generate" ? "active" : ""
+                    }`}
+                    to="/generate"
+                    aria-current={
+                      location.pathname === "/generate" ? "page" : undefined
+                    }
+                  >
+                    Generate
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
