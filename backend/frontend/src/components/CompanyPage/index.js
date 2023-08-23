83a99a0a598c22cace6f382847151d7c2274fe99
Julian Cardone <juliancardone1@gmail.com>
2023-08-23 16:58:42 -0400

    create custom hook for non-real time data

--- backend/frontend/src/components/CompanyPage/index.js
+++ backend/frontend/src/components/CompanyPage/index.js
@@ -8,13 +8,25 @@ function CompanyPage({ currentSymbol }) {
 
   useEffect(() => {
     fetchRealTimeData(currentSymbol);
-    const searchInfoInterval = setInterval(() => {
-      fetchRealTimeData(currentSymbol);
-    }, 5000);
 
-    return () => {
-      clearInterval(searchInfoInterval);
-    };
+    const currentTime = new Date();
+    const isMarketOpen =
+      currentTime.getDay() >= 1 &&
+      currentTime.getDay() <= 5 && // Monday to Friday
+      currentTime.getHours() >= 9 &&
+      currentTime.getHours() < 16; // 9:00am to 3:59pm
+
+    if (isMarketOpen) {
+      fetchRealTimeData(currentSymbol);
+
+      const searchInfoInterval = setInterval(() => {
+        fetchRealTimeData(currentSymbol);
+      }, 5000);
+
+      return () => {
+        clearInterval(searchInfoInterval);
+      };
+    }
   }, [currentSymbol, fetchRealTimeData]);
 
   return (
