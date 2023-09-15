1d5cf4e2e58d7789cc383062001eb53e01227502
Julian Cardone <juliancardone1@gmail.com>
2023-09-15 15:48:16 -0400

    style changes

--- backend/frontend/src/components/Home/WelcomeSection/ExchangeDisplay/index.js
+++ backend/frontend/src/components/Home/WelcomeSection/ExchangeDisplay/index.js
@@ -3,7 +3,7 @@ import ExchangeDisplayItem from "./ExchangeDisplayItem";
 function ExchangesDisplay({ realTimeHomePageData }) {
   return (
     <>
-      <div className="container-fluid">
+      <div className="container-fluid fadeIn animated-long">
         <div className="row d-flex-grid gap-4 justify-content-between align-items-center pt-5 pt-xxl-0 pt-xl-0 pt-lg-0 pt-md-0 px-5">
           {realTimeHomePageData.map((item, key) => {
             return (
---
+++ backend/frontend/src/components/Home/WelcomeSection/index.js
@@ -0,0 +1,35 @@
+import ExchangesDisplay from "./ExchangeDisplay";
+
+function WelcomeDiv({ loading, realTimeHomePageData }) {
+  return (
+    <>
+      <div className="container-fluid bg-primary text-white py-5">
+        <div className="container py-3">
+          <div className="row">
+            <div className="col-md-6">
+              <h1 className="fadeInLeft animated-short home-header pb-3">
+                welcome to your market analysis hub
+              </h1>
+              <p className="fadeInRight animated-long">
+                My mission is to make financial data accessible and
+                understandable for everyone. With our full-stack financial
+                analysis application, you can effortlessly input a stock symbol
+                of a public company and gain access to a treasure trove of
+                financial information. Whether you're an investor looking to
+                make informed decisions or simply curious about a company's
+                financial health, our platform is your go-to resource.
+              </p>
+            </div>
+            <div className="col-md-6 d-flex align-items-center">
+              {!loading && (
+                <ExchangesDisplay realTimeHomePageData={realTimeHomePageData} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default WelcomeDiv;
